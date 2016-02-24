bcbcf2a2bd6adfd735efea1c6f24cf841cace37a
Dommert <dommertenterprises@gmail.com>
2016-02-24 02:58:13 -0700

    Update routes.js

--- site/assets/js/routes.js
+++ site/assets/js/routes.js
@@ -1,1 +1,9 @@
-var foundationRoutes = [{"name":"settings","url":"/settings-this-is-cool","animationIn":"slideFromLeft","animationOut":"slideFromLeft","path":"templates/settings.html"},{"name":"home","url":"/","path":"templates/home.html"}]; 
+var foundationRoutes = [{"name":"settings",
+  "url":"/settings-this-is-cool",
+  "animationIn":"slideFromLeft",
+  "animationOut":"slideFromLeft",
+  "path":"templates/settings.html"
+},
+{"name":"home",
+  "url":"/",
+  "path":"templates/home.html"}]; 
